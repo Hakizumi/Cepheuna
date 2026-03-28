25ba0a8805121a1d6764e257cabc65e07bb79118
Hakizumi <Hakizumi@outlook.com>
2026-03-28 16:11:54 +0800

    feat: Added page redirection feature

--- src/main/java/org/hakizumi/cepheuna/config/WebConfig.java
+++ src/main/java/org/hakizumi/cepheuna/config/WebConfig.java
@@ -1,15 +1,23 @@
 package org.hakizumi.cepheuna.config;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+    public void addCorsMappings(@NotNull CorsRegistry registry) {
+        registry.addMapping("/backend/chat/**")
                 .allowedOrigins("*")
-                .allowedMethods("*");
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedHeaders("*");
+    }
+
+    @Override
+    public void addViewControllers(@NotNull ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/index.html", "/chat");
     }
 }
